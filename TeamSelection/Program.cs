f70bfbd61377fba76b3fc100e0e096787f8f9567
Niels Verbraeken <niels_verbraeken@hotmail.com>
2020-03-04 09:22:39 +0100

    Add project files.

---
+++ TeamSelection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamSelection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
